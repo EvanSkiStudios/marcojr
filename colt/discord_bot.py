5191f2b1217c35d35a5ad84f4a70bb76a5272857
Evanski <61751485+EvanSkiStudios@users.noreply.github.com>
2025-09-15 21:23:00 -0400

    Refactor config and improve logging in discord bot
    Replaces scattered environment variable usage with a structured CONFIG namespace for better maintainability. Updates command prefix, improves logging throughout the bot, and fixes issues with channel blacklist and message content handling. Also corrects the parrot command registration and cleans up message mention handling.

--- colt/discord_bot.py
+++ colt/discord_bot.py
@@ -1,5 +1,6 @@
 import os
 import re
+import random
 
 import discord
 
@@ -8,8 +9,7 @@ import discord_slash_commands as sc
 
 from discord.ext import commands
 from dotenv import load_dotenv
-
-import random
+from types import SimpleNamespace
 
 from discord_bot_users_manager import handle_bot_message
 from tools.elevenlabs_voice import text_to_speech
@@ -23,42 +23,52 @@ logger = setup_logger(__name__)
 
 # Load Env
 load_dotenv()
-BOT_TOKEN = os.getenv("TOKEN")
-BOT_APPLICATION_ID = os.getenv("APPLICATION_ID")
 
-BOT_SERVER_ID = os.getenv("GMCD_SERVER_ID")
-BOT_TEST_SERVER_ID = os.getenv("TEST_SERVER_ID")
-BOT_DM_CHANNEL_ID = os.getenv("DM_CHANNEL_ID")
-BOT_CHANNEL_ID = os.getenv("GMCD_CHANNEL_ID")
 
-MASTER_USER_ID = os.getenv("MASTER_USER_ID")
+def ns(d: dict) -> SimpleNamespace:
+    """Convert dict into a dot-accessible namespace (recursively)."""
+    return SimpleNamespace(**{k: ns(v) if isinstance(v, dict) else v for k, v in d.items()})
 
-GMC_DISCUSSION_THREAD = os.getenv("GMCD_NOT_ALLOWED_THREAD_D")
-GMC_NO_CONTEXT_THREAD = os.getenv("GMCD_NOT_ALLOWED_THREAD_NC")
-GMC_DANEEL_THREAD = os.getenv("GMCD_DANEEL_STINKY")
 
-channels_blacklist = [GMC_DISCUSSION_THREAD, GMC_NO_CONTEXT_THREAD, GMC_DANEEL_THREAD]
+config_dict = {
+    "BOT": {
+        "TOKEN": os.getenv("TOKEN"),
+        "APPLICATION_ID": os.getenv("APPLICATION_ID"),
+        "SERVER_ID": os.getenv("GMCD_SERVER_ID"),
+        "TEST_SERVER_ID": os.getenv("TEST_SERVER_ID"),
+        "DM_CHANNEL_ID": os.getenv("DM_CHANNEL_ID"),
+        "CHANNEL_ID": os.getenv("GMCD_CHANNEL_ID"),
+    },
+    "THREADS": {
+        "DISCUSSION": os.getenv("GMCD_NOT_ALLOWED_THREAD_D"),
+        "NO_CONTEXT": os.getenv("GMCD_NOT_ALLOWED_THREAD_NC"),
+        "DANEEL": os.getenv("GMCD_DANEEL_STINKY"),
+    },
+    "MASTER_USER_ID": os.getenv("MASTER_USER_ID"),
+}
+CONFIG = ns(config_dict)
+
+channels_blacklist = [int(CONFIG.THREADS.DISCUSSION), int(CONFIG.THREADS.NO_CONTEXT), int(CONFIG.THREADS.DANEEL)]
 
 # set discord intents
 intents = discord.Intents.default()
 intents.message_content = True
-intents.emojis = True
 intents.emojis_and_stickers = True
 
-command_prefix = "M! "
+command_prefix = "🔫! "
 client = commands.Bot(
     command_prefix=command_prefix,
     intents=intents,
     status=discord.Status.online
 )
-guild = discord.Object(id=BOT_SERVER_ID)
+guild = discord.Object(id=CONFIG.BOT.SERVER_ID)
 
 
 class MyHelpCommand(commands.HelpCommand):
     async def send_bot_help(self, mapping):
-        help_message = """
+        help_message = f"""
 For Full documentation see: [The Github Repo](<https://github.com/EvanSkiStudios/marcojr>)
-Commands are issued like so: `M! <command> <argument>`
+Commands are issued like so: `{command_prefix}<command> <argument>`
 ```Here are my commands:
 """
         for cog, commands_list in mapping.items():
@@ -79,29 +89,24 @@ COLT_Create()
 @client.event
 async def on_ready():
     # When the bot has logged in, call back
-    print(f'We have logged in as {client.user}')
+    logger.info(f'We have logged in as {client.user}')
     await client.tree.sync(guild=guild)
     # global
     try:
         synced = await client.tree.sync()
-        print(f"Synced {len(synced)} global command(s)")
+        logger.debug(f"Synced {len(synced)} global command(s)")
     except Exception as e:
-        print(e)
+        logger.error(e)
 
 
 @client.event
 async def on_disconnect():
-    print(f"{client.user} disconnected!")
+    logger.error(f"{client.user} disconnected!")
 
 
 @client.event
 async def on_connect():
-    print(f"{client.user} connected!")
-
-
-@client.event
-async def on_close():
-    print(f"{client.user} closed!")
+    logger.info(f"{client.user} connected!")
 
 
 # ------- BOT COMMANDS ----------
@@ -117,20 +122,21 @@ async def delete(ctx, *, arg=None):
 
 @client.command(help="Sanity Check for input")
 async def ping(ctx):
-    await ctx.send(f"Pong!")
+    await ctx.send("Pong!")
 
 
 # ------- SLASH COMMANDS ----------
 # noinspection PyUnresolvedReferences
 @client.tree.command(name="draw", description="Ping test", guild=guild)
 async def draw(interaction: discord.Interaction):
+    logger.debug(f'Command issued: draw by {interaction.user}')
     await interaction.response.send_message("Pew Pew! 🔥🔫")
 
 
 # noinspection PyUnresolvedReferences
 @client.tree.command(name="search", description="search internet", guild=guild)
 async def search(interaction: discord.Interaction, query: str):
-    # tell Discord we are working on it
+    logger.debug(f'Command issued: search by {interaction.user}, {query}')
     await interaction.response.defer()  # shows "Bot is thinking..."
 
     response = await llm_internet_search(f"search the web for: {query}")
@@ -140,6 +146,7 @@ async def search(interaction: discord.Interaction, query: str):
 # noinspection PyUnresolvedReferences
 @client.tree.command(name="tts", description="text to speech", guild=guild)
 async def tts(interaction: discord.Interaction, text: str):
+    logger.debug(f'Command issued: tts by {interaction.user}, {text}')
     await interaction.response.defer()
 
     tts_response = await text_to_speech(text, file_name=text)
@@ -160,8 +167,10 @@ async def tts(interaction: discord.Interaction, text: str):
 
 # noinspection PyUnresolvedReferences
 @client.tree.command(name="parrot", description="speak", guild=guild)
-async def search(interaction: discord.Interaction, text: str):
-    if str(interaction.user.id) != MASTER_USER_ID:
+async def parrot(interaction: discord.Interaction, text: str):
+    logger.debug(f'Command issued: parrot by {interaction.user}, {text}')
+
+    if interaction.user.id != int(CONFIG.MASTER_USER_ID):
         await interaction.response.send_message("Squawk!")
     else:
         await interaction.response.send_message(f"{text}")
@@ -170,7 +179,8 @@ async def search(interaction: discord.Interaction, text: str):
 # noinspection PyUnresolvedReferences
 @client.tree.command(name="neuralize", description="Empties the conversation cache", guild=guild)
 async def neuralize(interaction: discord.Interaction):
-    if str(interaction.user.id) != MASTER_USER_ID:
+    logger.debug(f'Command issued: neuralize by {interaction.user}')
+    if interaction.user.id != int(CONFIG.MASTER_USER_ID):
         await interaction.response.send_message("The inner mechanisms of my mind are an enigma")
     else:
         colt_current_session_chat_cache.clear()
@@ -204,7 +214,7 @@ async def llm_chat(message, username, user_nickname, message_content):
     async with message.channel.typing():
         message_is_request = is_search_request(message_content)
         if message_is_request:
-            logger.info(f"Message is a Internet Search")
+            logger.info("Message is a Internet Search")
             response = await llm_internet_search(message_content)
         else:
             response = await COLT_Message(username, user_nickname, message_content)
@@ -213,6 +223,7 @@ async def llm_chat(message, username, user_nickname, message_content):
         return
 
     # response should have been split in the above function returns
+    sent_message = None
     for i, part in enumerate(response):
         if not message.author.bot and i == 0:
             sent_message = await message.reply(part, suppress_embeds=True)
@@ -235,29 +246,21 @@ async def llm_chat(message, username, user_nickname, message_content):
 
 @client.event
 async def on_message(message):
-    if str(message.channel.id) in channels_blacklist:
+    if message.channel.id in channels_blacklist:
         return
     
     await client.process_commands(message)
 
-    message_content = message.content
+    message_content = message.clean_content
     username = message.author.name
     user_nickname = message.author.display_name
-
-    for person in message.mentions:
-        message_content = message_content.replace(f"<@{person.id}>", f"@{person.name}")
-        message_content = message_content.replace(f"<@!{person.id}>", f"@{person.name}")
 
     channel = client.get_channel(message.channel.id)
     if not colt_current_session_chat_cache:
         async for past_message in channel.history(limit=20):
             author_name = past_message.author.name
             author_nick = past_message.author.display_name
-            content = past_message.content
-
-            for user_ref in past_message.mentions:
-                content = content.replace(f"<@{user_ref.id}>", f"@{user_ref.name}")
-                content = content.replace(f"<@!{user_ref.id}>", f"@{user_ref.name}")
+            content = past_message.clean_content
 
             if past_message.author == client.user:
                 message_prompt = {"role": "assistant", "content": f'{content}'}
@@ -285,7 +288,6 @@ async def on_message(message):
 
     # DMs
     if isinstance(message.channel, discord.DMChannel):
-        # print(f"{message_content}")
         await llm_chat(message, username, user_nickname, message_content)
         return
 
@@ -293,18 +295,16 @@ async def on_message(message):
     if message.reference:
         referenced_message = await message.channel.fetch_message(message.reference.message_id)
         if referenced_message.author == client.user:
-            message_content = message_content.replace(f"<@{BOT_APPLICATION_ID}>", "")
             await llm_chat(message, username, user_nickname, message_content)
             return
 
     # ping
     if client.user.mentioned_in(message):
-        message_content = message_content.replace(f"<@{BOT_APPLICATION_ID}>", "Colt ")
         await llm_chat(message, username, user_nickname, message_content)
         return
 
     # if the message includes "colt " it will trigger and run the code
-    if re.search(r"\bcolt[\s,.?!]", message_content.lower()):
+    if re.search(r"\bcolt[\s,.?!]", message_content, re.IGNORECASE):
         await llm_chat(message, username, user_nickname, message_content)
         return
 
@@ -314,4 +314,4 @@ async def on_message(message):
 
 
 # Startup discord Bot
-client.run(BOT_TOKEN)
+client.run(CONFIG.BOT.TOKEN)
